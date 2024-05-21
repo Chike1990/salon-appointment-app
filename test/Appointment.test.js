e64509c29b3d712c18cf2e69383cc5d4d5ea6251
chibuike Uzoechina <chibuikeuzoechina@gmail.com>
2024-05-22 03:58:05 +1200

    test setup

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,11 @@
-describe("Appointment", () =>{
-    it("renders customer firstname", () =>{
-        expect(document.body.textContent).toContain("Chike")
-    })
-})
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import {Appointment} from "../src/Appointment"
+it("renders the customer first name", () => {
+  const customer = { firstName: "Ashley" };
+  const component = <Appointment customer={customer} />;
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+  ReactDOM.createRoot(container).render(component);
+  expect(document.body.textContent).toContain("Ashley");
+});
